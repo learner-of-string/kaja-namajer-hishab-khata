7a59086aec6ae75dafcf6a9acd432d2c6f1940c1
Learner of String <aref0171personal@gmail.com>
2025-09-25 19:55:06 +0600

    replace mosque icon with favicon image in Header component

--- kaja-namajer-hishab/src/Components/Header.jsx
+++ kaja-namajer-hishab/src/Components/Header.jsx
@@ -1,9 +1,11 @@
-import { FaMosque } from "react-icons/fa6";
-
 const Header = () => {
     return (
         <div className="flex flex-col gap-2 justify-center items-center">
-            <FaMosque className="text-orange-500 md:text-7xl text-4xl text-center" />
+            <img
+                src="/favicon.png"
+                alt="kaja namaj volume"
+                className="size-20"
+            />
             <h1 className="md:text-5xl text-4xl text-center font-bold font-nunito">
                 Kaja Namaz Volume
             </h1>
